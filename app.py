0d6017b9a7c712a9fc16487536a18eb73c0009c0
Robim-rur <cruvinelebook@gmail.com>
2026-05-17 08:58:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -646,14 +646,14 @@ if not df_ops.empty:
         format_func=lambda x: opcoes_operacoes[x]
     )
 
-  linha = df_ops[df_ops["ID"] == operacao_id].iloc[0]
+     linha = df_ops[df_ops["ID"] == operacao_id].iloc[0]
 
-# =====================================================
-# DETALHES DA OPERAÇÃO
-# =====================================================
+    # =====================================================
+    # DETALHES DA OPERAÇÃO
+    # =====================================================
 
-st.info(
-    f"""
+    st.info(
+        f"""
 📌 Ticker: {linha['Ticker']}
 
 📅 Data Compra: {linha['Data Compra']}
@@ -668,31 +668,10 @@ st.info(
 
 🧠 Estratégia: {linha['Estratégia']}
 """
-)
+    )
 
-st.markdown("### ✏️ Editar Operação")
+    st.markdown("### ✏️ Editar Operação")
 
-# =====================================================
-# DETALHES DA OPERAÇÃO
-# =====================================================
-
-st.info(
-    f"""
-📌 Ticker: {linha['Ticker']}
-
-📅 Data Compra: {linha['Data Compra']}
-
-📦 Quantidade: {linha['Qtd']}
-
-💰 Preço Compra: R$ {linha['Preço Compra']:.2f}
-
-🎯 Alvo: R$ {linha['Alvo (3%)']:.2f}
-
-📊 Status: {linha['Status']}
-
-🧠 Estratégia: {linha['Estratégia']}
-"""
-)
     col1, col2, col3 = st.columns(3)
 
     with col1:
