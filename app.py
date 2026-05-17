9481cbf2bc757e0852c61feceff3a973cdb3d783
Robim-rur <cruvinelebook@gmail.com>
2026-05-17 07:28:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,17 +47,9 @@ def conectar_planilha():
 
     planilha = client.open_by_key(SHEET_ID)
 
-try:
     aba = planilha.worksheet(ABA_NOME)
 
-except:
-    aba = planilha.add_worksheet(
-        title=ABA_NOME,
-        rows=1000,
-        cols=20
-    )
-
-return aba
+    return aba
 
 
 # =========================================================
